f4fe297da9eb74fd49d531c698c4a892591a32f8
Paul <paulhyunchong@gmail.com>
2021-09-17 23:06:57 -0700

    initial commit from starter next.js blog example

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,17 @@
 import 'nextra-theme-blog/style.css'
+import { usePanelBear } from '../hooks/usePanelbear'
 import Head from 'next/head'
 
-import '../styles/main.css'
+import 'nextra-theme-blog/style.css'
 
 export default function Nextra({ Component, pageProps }) {
+  usePanelBear(process?.env?.NEXT_PUBLIC_PANEL_BEAR_ID, {
+    // Uncomment to allow sending events on localhost, and log to console too.
+    debug: false
+  })
   return (
     <>
       <Head>
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
         <link
           rel="preload"
           href="/fonts/Inter-roman.latin.var.woff2"
--- pages/_document.js
+++ pages/_document.js
@@ -8,10 +8,9 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
-      image:
-        'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+      title: 'Paul Chong | Front End Engineer',
+      description:
+        'Paul Chong is a software engineer who specializes in front-end and UI development.'
     }
 
     return (
@@ -22,12 +21,10 @@ class MyDocument extends Document {
           <meta property="og:site_name" content={meta.title} />
           <meta property="og:description" content={meta.description} />
           <meta property="og:title" content={meta.title} />
-          <meta property="og:image" content={meta.image} />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@yourname" />
           <meta name="twitter:title" content={meta.title} />
           <meta name="twitter:description" content={meta.description} />
-          <meta name="twitter:image" content={meta.image} />
         </Head>
         <body>
           <Main />
