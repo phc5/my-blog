d20e3ca31ab0d3b47c3147c9e54be708aba3f492
Paul <paulhyunchong@gmail.com>
2021-09-18 20:44:55 -0700

    Adds redirect for vscode high memory usage

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+module.exports = withNextra({
+  async redirects() {
+    return [
+      {
+        source: '/blog/technology/vscode-high-memory-usage',
+        destination: '/posts/vscode-high-memory-usage',
+        permanent: true
+      }
+    ]
+  }
+})
